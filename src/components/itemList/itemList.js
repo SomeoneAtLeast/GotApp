a71b40d6570337b757a76d41fcf3ecee459c9d71
Вадим Бабичев <WineCooler@yandex.ru>
2021-02-25 18:08:47 +0300

    Хуки

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,57 +1,53 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import Spinner from '../spinner';
 import './itemList.css';
 
-export default class ItemList extends Component {
+function ItemList ({getData, onItemSelected, renderItem}) {
+    // хук со стейтом
+    //itemList (стайт) - [] вот он
+    // updateList - кастоманая функция для изменения стейта
+    const [itemList, updateList] = useState([])
 
-    state = {
-        itemList: null
-    }
-
-    componentDidMount() {
-        const {getData} = this.props;
-        // получаем данные и заносим в стейт
+    // заменят жизненные циклы (componentDidMount() и тп), создание, обновление, удаление.
+    useEffect(() => {
         getData()
-            .then((itemList) => {
-                this.setState({
-                    itemList
-                })
+            .then((data) => {
+                updateList(data)
             })
-    }
+    }, []) // [] - говорит о том, что выполнять нужно только при появлении и исчезновении, но не при обновление. Это чтобы не было кучи запросов и утечки памяти
 
-    renderItems(arr) {
+
+    function renderItems(arr) {
         // item - объект, который помещаем на страницу. 
         return arr.map((item, i) => {
             const {id} = item;
             // здесь получаем то, что запросили в charactePage
             // renderItem={(item) => item.name}
-            const label = this.props.renderItem(item);
+            const label = renderItem(item);
             return (
             <li 
                 className="list-group-item"
                 key={id}
-                onClick={() => this.props.onItemSelected(id)}
+                onClick={() => onItemSelected(id)}
                 >
                 {label}
             </li>
             )
         })
     }
-    render() {
-
-        const {itemList} = this.state;
 
         // если персонажей нет - пусти спиннер.
         if (!itemList) {
             return <Spinner/>
         }
 
-        const items = this.renderItems(itemList)
+        const items = renderItems(itemList)
 
         return (
             <ul className="item-list list-group">
                 {items}
             </ul>
         );
-    }
 }
+
+export default ItemList;
